c75dd4a715768bd76a1045ab9d7148498e947339
Dimitar Klaturov <bulgaria_mitko@yahoo.com>
2016-01-26 17:24:26 +0200

    added some more questions...

--- Team-BaconJS-TeamWork/scripts/questions.js
+++ Team-BaconJS-TeamWork/scripts/questions.js
@@ -1,11 +1,26 @@
 var quiz = [{
-  "question": "What is the capital of Bulgaria from 972 till 992 year?",
-  "choices": ["Veliko Tarnovo", "Pliska", "Sofia", "Skopje"],
-  "correct": "Skopje",
-  "money" : "1000"
-}, {
-  "question": "Which Bulgarian TV Show have more then 750 episodes?",
+  "question": "Which Bulgarian TV Show has more then 750 episodes?",
   "choices": ["Kato dve kapki voda", "Palna Ludnica", "Minuta e mnogo", "Ulicata"],
   "correct": "Minuta e mnogo",
+  "money" : "1000"
+}, {
+  "question": "What is the max speed you can achive with the car Tesla Model S P85 D?",
+  "choices": ["150km", "200lm", "240km", "300km"],
+  "correct": "240km",
   "money" : "2500"
+}, {
+  "question": "Which is fastest?",
+  "choices": ["DDRam", "SSD Hard Drive", "Cloud (GDrive, Dropbox, Box.net and ect)", "HHD"],
+  "correct": "DDR",
+  "money" : "3000"
+}, {
+  "question": "What is the name of the first person who discover photography?",
+  "choices": ["Caroline Rudolphi", "Ludwig Heinrich von Jakob", "Johann Heinrich Schulze", "Carl von Clausewitz"],
+  "correct": "Johann Heinrich Schulze",
+  "money" : "4000"
+}, {
+  "question": "What was the capital of Bulgaria from 972 till 992 year?",
+  "choices": ["Veliko Tarnovo", "Pliska", "Sofia", "Skopje"],
+  "correct": "Skopje",
+  "money" : "10000"
 }];
